88e1afcf9fc52f87b5032bc30b0ce976fe0c64c8
Octavio Benitez <gtstudent95@gmail.com>
2024-01-24 09:56:29 -0700

    changing debugs, correcting github run command

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import pytest
 import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 def test_verify_txt_file():
     filename = 'output.txt' 
-    print('Verifying file exists...')
+    logging.debug('Verifying file exists...')
     try:
         assert os.path.exists(filename)
-        print('File exists.')
+        logging.debug('File exists.')
     except AssertionError:
-        print("File does not exist")
+        logging.debug("File does not exist")
 
     with open(filename, 'r') as file:
         data = file.read()
-        print('Verifying file content...')
+        logging.debug('Verifying file content...')
         try:
             assert 'Hello!' in data
-            print('Expected string found in file.')
+            logging.debug('Expected string found in file.')
         except AssertionError:
-            print("Expected string not found in file")  
+            logging.debug("Expected string not found in file")  
 
 if __name__ == '__main__':
     test_verify_txt_file()
